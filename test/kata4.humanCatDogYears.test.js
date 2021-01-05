09afd5dbcf60cb95db51a22ed42fb1931b561fad
batmanba <berke.batman@cass.city.ac.uk>
2021-01-05 23:25:22 +0000

    reachDesetination completed

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,7 @@
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+  it("returns human/cat/dog year", () => {
+    expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+  });
+});

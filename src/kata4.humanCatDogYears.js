186dc26c5055dc6f2e8833935de921af69215183
batmanba <berke.batman@cass.city.ac.uk>
2021-01-05 23:16:56 +0000

    human cat dog completed

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,13 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = number => {
+    humanYear = number; 
+
+    catYear = 15 + 9 + ((number - 2)*4);
+
+    dogYear = 15 + 9 + ((number - 2)*5);
+    x = [humanYear, catYear, dogYear];
+    console.log(x)
+    return x;
+
+};
 
 module.exports = humanCatDogYears;

d058d81a64e3f0ca1acb9761e1e1d735f47b4b09
batmanba <berke.batman@cass.city.ac.uk>
2021-01-05 22:51:02 +0000

    kata2 completed

--- test/kata1.fizzBuzz.test.js
+++ test/kata1.fizzBuzz.test.js
@@ -1,13 +1,31 @@
 const { fizzBuzz } = require("../src");
 
 describe("fizzBuzz", () => {
-  test("returns Fizz when passed a multiple of 3", () => {});
+  it("returns Fizz when passed a multiple of 3", () => {
+    const result = fizzBuzz(3);
 
-  test("returns Buzz when passed a multiple of 5", () => {});
+    expect(result).toBe('Fizz');
 
-  test("returns FizzBuzz when passed a multiple 3 and 5", () => {});
+  });
 
-  test("returns the number when it isn't a multiple of 3 or 5", () => {});
+  it("returns Buzz when passed a multiple of 5", () => {
+    const result = fizzBuzz(5);
+
+    expect(result).toBe('Buzz');
+
+  });
+
+  it("returns FizzBuzz when passed a multiple 3 and 5", () => {
+    expect(fizzBuzz(15)).toBe('FizzBuzz');
+    expect(fizzBuzz(30)).toBe('FizzBuzz');
+    expect(fizzBuzz(45)).toBe('FizzBuzz');
+    expect(fizzBuzz(60)).toBe('FizzBuzz');
+  });
+
+  it("returns the number when it isn't a multiple of 3 or 5", () => {
+    expect(fizzBuzz(16)).toBe(16);
+    expect(fizzBuzz(13)).toBe(13);
+    expect(fizzBuzz(101)).toBe(101);
+    expect(fizzBuzz(14)).toBe(14);
+  });
 });
-
-
